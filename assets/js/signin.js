3bf6c181a86cf3c7898f165ef8be747a11881b48
ehsan sayyadi <ehsansayadi7189@gamil.com>
2025-02-18 14:30:05 +0330

    feat:handing incorrect sign in information

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,8 +1,45 @@
-const subBtn = document.querySelector("button#signin-btn");
+const subBtn = document.getElementById("signin-btn");
+const email = document.getElementById('floatingInput');
+const pass = document.getElementById('password');
+const incorrectMassage = document.getElementById('massage');
+
+// show massage function 
+
+const showMassage = element => element.classList.remove("d-none")
+
+// add danger class
+const dangerClass = elementName => elementName.classList.add("bg-danger");
+// sending to denger class
+
+const sendToDanger = (...args) => args.map(item => dangerClass(item));
 
 
+// !change input bg
 
+const incorrectHandling = () => {
+    sendToDanger(pass, email)
+    showMassage(incorrectMassage)
+}
+
+// !change Page functionality
+
+const changePage = () => {
+    window.location.href = "../pages/check-out.html"
+}
+
+
+// ! sub btn functionality
 subBtn.addEventListener("click", e => {
     e.preventDefault();
+    !email.value && !pass.value ? incorrectHandling() : changePage();
+
+
+
+
+    // });
 
 })
+
+
+
+
