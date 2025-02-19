7e2ee4931cf112b203623e681a5d7519bb2fd862
ehsan sayyadi <ehsansayadi7189@gamil.com>
2025-02-19 22:02:14 +0330

    feat:select form element

---
+++ assets/js/checkOut.js
@@ -0,0 +1,16 @@
+const elementSelector = id => document.getElementById(id);
+
+const firstName = elementSelector("firstName");
+const lastName = elementSelector("lastName");
+const userName = elementSelector("username");
+const emailContainer = elementSelector("email");
+const passContainer = elementSelector("password");
+const gitLink = elementSelector("gitLink");
+const countryCode = elementSelector("countryCode");
+const phoneNumber = elementSelector("phoneNumber");
+const skill = elementSelector("skill");
+
+
+
+
+
