fed3db3ea592d4e939d784925f8155b0261a5b86
ehsan sayyadi <ehsansayadi7189@gamil.com>
2025-02-26 21:21:12 +0330

    feat:seting fotns for products

--- assets/js/checkOut.js
+++ assets/js/checkOut.js
@@ -9,8 +9,80 @@ const gitLink = elementSelector("gitLink");
 const countryCode = elementSelector("countryCode");
 const phoneNumber = elementSelector("phoneNumber");
 const skill = elementSelector("skill");
+const freelancerBtn = elementSelector("freelancer");
+const clientBtn = elementSelector("client");
+const typeOfUser = freelancerBtn.checked ? freelancerBtn.value : clientBtn.value;
+const cardOwner = elementSelector("cc-name")
+const cardNumber = `${elementSelector("cc-number").value.substring(0, 4)}-${elementSelector("cc-number").value.substring(4, 8)}-${elementSelector("cc-number").value.substring(8, 12)}-${elementSelector("cc-number").value.substring(12)}`
+console.log(cardNumber);
 
 
 
 
 
+
+
+let user = JSON.parse(localStorage.getItem("user"));
+
+// !set email and pass
+
+emailContainer.value = user.email;
+passContainer.value = user.pass;
+
+// !this data send to will server
+const userData = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    userName: userName.value,
+    email: emailContainer.value,
+    password: passContainer.value,
+    gitLink: gitLink.value,
+    countryCode: countryCode.value,
+    phoneNumber: phoneNumber.value,
+    skill: skill.value,
+
+
+};
+
+
+
+
+// !
+
+(() => {
+
+
+
+    fetch("http://localhost:3000/country")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("network was not ok" + res.statusText);
+            };
+            return res.json();
+        })
+        .then(data => data.map(item => {
+            const option = document.createElement("option");
+            option.value = `${item.numericCode}`
+            option.innerText = `${item.name}(+${item.numericCode})`
+            countryCode.appendChild(option)
+        }));
+
+    fetch("http://localhost:3000/freelancerSkills")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("network was not ok" + res.statusText);
+            };
+            return res.json();
+        })
+        .then(data => data.map(item => {
+            const option = document.createElement("option");
+            option.value = `${item}`;
+            option.innerText = `${item}`;
+            skill.appendChild(option);
+        }));
+
+    // !send user data to server
+
+
+
+})()
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 const productContainer = document.getElementById('product-container');
 
-
-
+const shortText = (text, maxLength = 136) => text.length > maxLength ? `${(text.substring(0, maxLength))}` + "..." : text + "..."
 
 // !mapping data
 
 const iteration = rawData => {
     rawData.map(item => {
         const cardContainer = document.createElement("div");
-        cardContainer.classList.add("card", "col-lg-4");
+        cardContainer.classList.add("card", "col-lg-4", "product");
+
         cardContainer.innerHTML = `
         
-        <img src="./assets/img/826118.png" class="card-img-top" alt="..." />
-        <div class="card-body">
-            <h5 class="card-title"> ${item.title}</h5>
-            <p class="card-text">
-                ${item.description}
+        <img src="./assets/img/826118.png" class="card-img-top product__image" alt="..." />
+        <div class="card-body product__body">
+            <h3 class="card-title  product__title"> ${item.title}</h3>
+            <p class="card-text product__description">
+                ${shortText(item.description)}
             </p>
-            <a href="#" class="btn sign-btn" >Go somewhere</a>
+            <a href="#" class="btn sign-btn product-btn" >Go somewhere</a>
         </div>
         `;
         productContainer.appendChild(cardContainer);
